ecc08130af2a2c1ccf9a5b0793be8f679321e89b
Ariel vaknin <arielhillel77@gmail.com>
2022-04-10 18:18:21 +0300

    Create UI for create test page

--- frontend/src/components/Tests/CreateTest.tsx
+++ frontend/src/components/Tests/CreateTest.tsx
@@ -1,64 +1,137 @@
-import React from 'react';
-import { useState } from 'react';
-import { TextInput, Group, Button, SimpleGrid, Modal } from '@mantine/core';
-import ChooseQuestion from '../Questions/ChooseQuestion';
+import React from "react";
+import { useState } from "react";
+import {
+  TextInput,
+  Group,
+  Button,
+  SimpleGrid,
+  Modal,
+  MultiSelect,
+} from "@mantine/core";
+import ChooseQuestion from "../Questions/ChooseQuestion";
+import { Hash } from "tabler-icons-react";
 
 function CreateTest() {
-    const [openedNQ, setOpenedNQ] = useState(false);
-    const [openedEQ, setOpenedEQ] = useState(false);
+  const [openedNQ, setOpenedNQ] = useState(false);
+  const [openedEQ, setOpenedEQ] = useState(false);
+  const [dataHash, setHashData] = useState([
+    "React",
+    "C#",
+    "JavaScript",
+    "Python",
+  ]);
+  const [demoEmailsData, setDemoEmailsData] = useState([
+    "demo1@gmail.com",
+    "demo2@gmail.com",
+    "demo3@gmail.com",
+    "demo4@gmail.com",
+  ]);
 
-    return (
-        <>
-        <Modal
+  return (
+    <>
+      <Modal
         opened={openedNQ}
         onClose={() => setOpenedNQ(false)}
         title="New Question"
         size="75%"
-        >
-        {
-            "<NewQuestion/>"
-        }
+      >
+        {"<NewQuestion/>"}
       </Modal>
       <Modal
         opened={openedEQ}
         onClose={() => setOpenedEQ(false)}
         title="Questions Catalog"
         size="75%"
-        >
-        {
-            <ChooseQuestion/>
-        }
+      >
+        {<ChooseQuestion />}
       </Modal>
 
-        <SimpleGrid cols={1} spacing="xs">
-            <h1>New Test</h1>
-            <div>
-            <TextInput label="Test Name:" style={{ width: "40%", textAlign: 'left' }} placeholder="Test Name" radius="xs"/>
-            </div>
-            <div>
-            <TextInput label="Subject:" style={{ width: "40%", textAlign: 'left' }} placeholder="Subject" radius="xs"/>
-            </div>
-            <div>
-            <TextInput label="Tags:" style={{ width: "40%", textAlign: 'left' }} placeholder="#Tags" radius="xs"/>
-            </div>
-            <div>
-                <Group spacing="sm">
-                    <Button variant="outline" radius="lg" onClick={() => setOpenedNQ(true)}>
-                    ADD NEW QUESTION</Button>
-                    <Button variant="outline" radius="lg" onClick={() => setOpenedEQ(true)} color="dark" >CHOOSE FROM CATALOG</Button>
-                </Group>
-            </div>
-            <div>
-                <Group spacing="sm">
-                    <Button radius="lg" variant="gradient" gradient={{ from: '#838685', to: '#cfd0d0' }}>
-                    Save as Draft</Button>
-                    <Button radius="lg" variant="gradient" gradient={{ from: '#217ad2', to: '#4fbaee' }}>
-                    Publish</Button>
-                </Group>
-            </div>
-        </SimpleGrid>
-        </>
-    );
+      <SimpleGrid cols={1} spacing="xs">
+        <h1>New Test</h1>
+        <div>
+          <TextInput
+            label="Test Name:"
+            style={{ width: "40%", textAlign: "left" }}
+            placeholder="Test Name"
+            radius="xs"
+          />
+        </div>
+        <div>
+          <TextInput
+            label="Subject:"
+            style={{ width: "40%", textAlign: "left" }}
+            placeholder="Subject"
+            radius="xs"
+          />
+        </div>
+        <div>
+          <MultiSelect
+            data={dataHash}
+            label="Tags:"
+            style={{ width: "40%", textAlign: "left" }}
+            placeholder="#Tags"
+            radius="xs"
+            searchable
+            creatable
+            getCreateLabel={(query) => `+ Create ${query}`}
+            onCreate={(query) => setHashData((current) => [...current, query])}
+          />
+        </div>
+        <div>
+          <Group spacing="sm">
+            <Button
+              variant="outline"
+              radius="lg"
+              onClick={() => setOpenedNQ(true)}
+            >
+              ADD NEW QUESTION
+            </Button>
+            <Button
+              variant="outline"
+              radius="lg"
+              onClick={() => setOpenedEQ(true)}
+              color="dark"
+            >
+              CHOOSE FROM CATALOG
+            </Button>
+          </Group>
+        </div>
+        <div>
+          <MultiSelect
+            data={demoEmailsData}
+            label="Asigned to:"
+            style={{ width: "40%", textAlign: "left" }}
+            placeholder="Students emails"
+            radius="xs"
+            searchable
+            creatable
+            getCreateLabel={(query) => `+ Add ${query}`}
+            onCreate={(query) =>
+              setDemoEmailsData((current) => [...current, query])
+            }
+          />
+        </div>
+        <div>
+          <Group spacing="sm">
+            <Button
+              radius="lg"
+              variant="gradient"
+              gradient={{ from: "#838685", to: "#cfd0d0" }}
+            >
+              Save as Draft
+            </Button>
+            <Button
+              radius="lg"
+              variant="gradient"
+              gradient={{ from: "#217ad2", to: "#4fbaee" }}
+            >
+              Publish
+            </Button>
+          </Group>
+        </div>
+      </SimpleGrid>
+    </>
+  );
 }
 
-export default CreateTest;  
+export default CreateTest;
--- frontend/src/components/Tests/TestsPage.tsx
+++ frontend/src/components/Tests/TestsPage.tsx
@@ -1,28 +1,31 @@
-import React from 'react';
-import { AppShell, Header, Image } from '@mantine/core';
-import CreateTest from './CreateTest';
-import { Sidebar } from '../AppShell/SideBar';
+import React from "react";
+import { AppShell, Header, Image } from "@mantine/core";
+import CreateTest from "./CreateTest";
+import { Sidebar } from "../AppShell/SideBar";
 
 function TestsPage() {
-    return (
-        <AppShell
-        header={<Header height={80} p="xs">{
-          <div style={{ width: 150, height: 60, display: "fixed" }}>
-            <Image
-              radius="md"
-              src="https://www.zion-net.co.il/wp-content/uploads/2021/06/ZioNET-Logo_new_new-300x127.png"
-              alt="Zionet Logo"
-            />
-          </div>
-        }</Header>}
-        padding="md"
-        fixed navbar={<Sidebar/>}
-      >
-        {
-          <CreateTest/>
-        }
-      </AppShell>
-    );
+  return (
+    <AppShell
+      header={
+        <Header height={80} p="xs">
+          {
+            <div style={{ width: 150, height: 60, display: "fixed" }}>
+              <Image
+                radius="md"
+                src="https://www.zion-net.co.il/wp-content/uploads/2021/06/ZioNET-Logo_new_new-300x127.png"
+                alt="Zionet Logo"
+              />
+            </div>
+          }
+        </Header>
+      }
+      padding="md"
+      fixed
+      navbar={<Sidebar />}
+    >
+      {<CreateTest />}
+    </AppShell>
+  );
 }
 
 export default TestsPage;
