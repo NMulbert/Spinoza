1f2ea18b6ea9b1a44e706c5f6ec4252968154865
Ariel vaknin <arielhillel77@gmail.com>
2022-04-19 13:50:42 +0300

    Small change

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,16 +2,12 @@ import React from "react";
 import "./App.css";
 import { Route, Routes } from "react-router-dom";
 import Dashboard from "./components/Dashboards/Dashboard";
-import { AuthenticationTitle } from "./components/Login/Login";
-import { ForgotPassword } from "./components/Login/ForgotPassword";
 function App() {
   return (
     <div className="App">
       <Routes>
-        <Route path="/" element={<AuthenticationTitle />} />
+        <Route path="/" element={<Dashboard />} />
         <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/login" element={<AuthenticationTitle />} />
-        <Route path="/forgot-password" element={<ForgotPassword />} />
       </Routes>
     </div>
   );
