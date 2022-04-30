f394f88151461343bcb0e6bfc67264704ea1f8fb
Alon Fliess <alonf@codevalue.net>
2022-04-30 16:33:47 +0300

    Add local support for signalR Work in progress

---
+++ backend/Managers/SignalRConnectionSupport/SignalR.cs
@@ -0,0 +1,25 @@
+using System;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.Azure.WebJobs.Extensions.SignalRService;
+using Newtonsoft.Json;
+
+namespace SignalRConnectionSupport
+{
+    public static class SignalR
+    {
+        [FunctionName("negotiate")]
+        public static SignalRConnectionInfo Negotiate(
+            [HttpTrigger(AuthorizationLevel.Anonymous)] HttpRequest req,
+            [SignalRConnectionInfo(HubName = "serverless")] SignalRConnectionInfo connectionInfo)
+        {
+            return connectionInfo;
+        }
+    }
+}
