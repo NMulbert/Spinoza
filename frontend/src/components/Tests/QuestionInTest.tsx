a52f18a59fc6970a207c70585fd39600c561df78
DavidGarm <davidgerm1997@gmail.com>
2022-05-03 19:55:01 +0300

    modified preview for questions in test

--- frontend/src/components/Tests/QuestionInTest.tsx
+++ frontend/src/components/Tests/QuestionInTest.tsx
@@ -1,5 +1,6 @@
-import { Button, Card, Title } from "@mantine/core";
+import { Button, ActionIcon, Card, Grid, Title, Text } from "@mantine/core";
 import React from "react";
+import { X } from "tabler-icons-react";
 
 type QuestionData = {
   id: string;
@@ -7,7 +8,7 @@ type QuestionData = {
   editMode: boolean;
 };
 
-function QuestionInTest({ id, removeQuestion, editMode }: QuestionData) {
+function Preview({ id, removeQuestion, editMode }: QuestionData) {
   return (
     <Card
       withBorder
@@ -15,29 +16,53 @@ function QuestionInTest({ id, removeQuestion, editMode }: QuestionData) {
       p="lg"
       radius="xl"
       style={{
-        height: 309,
-        minHeight: 300,
+        height: "100%",
+        minHeight: "100%",
         width: "90%",
         minWidth: "90%",
         margin: "auto",
         display: "inline-block",
       }}
     >
-      <Title order={4} style={{ textAlign: "center" }}>
-        {id}
-      </Title>
       {editMode ? (
-        <Button
+        <ActionIcon
+          color="red"
+          variant="hover"
+          radius="lg"
+          style={{ float: "right", cursor: "pointer" }}
           onClick={() => {
             removeQuestion(id);
           }}
         >
-          X
-        </Button>
+          <X size={24} />
+        </ActionIcon>
       ) : (
         <></>
       )}
+
+      <Title order={3}>Question name {id}</Title>
+      <Text lineClamp={2}>
+        From Bulbapedia: Bulbasaur is a small, quadrupedal Pokémon that has
+        blue-green skin with darker patches. It has red eyes with white pupils,
+        pointed, ear-like structures on top of its head, and a short, blunt
+        snout with a wide mouth. A pair of small, pointed teeth are visible in
+        the upper jaw when its mouth is open.From Bulbapedia: Bulbasaur is a
+        small, quadrupedal Pokémon that has blue-green skin with darker patches.
+        It has red eyes with white pupils, pointed, ear-like structures on top
+        of its head, and a short, blunt snout with a wide mouth. A pair of
+        small, pointed teeth are visible in the upper jaw when its mouth is
+        open.
+      </Text>
+      <Button
+        size="xs"
+        radius="lg"
+        variant="light"
+        color="blue"
+        style={{ marginTop: 14, width: 120 }}
+      >
+        Open
+      </Button>
     </Card>
   );
 }
-export default QuestionInTest;
+export default Preview;
