ea4558453ba9667df8c14526eea3b70d23ff3f56
DavidGarm <davidgerm1997@gmail.com>
2022-05-13 15:20:40 +0300

    view questions in test temporary solution

--- frontend/src/components/Tests/QuestionInTest.tsx
+++ frontend/src/components/Tests/QuestionInTest.tsx
@@ -1,4 +1,16 @@
-import { Button, ActionIcon, Card, Grid, Title, Text, Group, Divider } from "@mantine/core";
+import {
+  Button,
+  ActionIcon,
+  Card,
+  Radio,
+  Title,
+  Text,
+  Group,
+  Divider,
+  Badge,
+  Checkbox,
+} from "@mantine/core";
+import MDEditor from "@uiw/react-md-editor";
 import React, { useEffect, useState } from "react";
 import { CalendarTime, Stars, UserCircle, X } from "tabler-icons-react";
 
@@ -8,17 +20,24 @@ type QuestionData = {
   editMode: boolean;
 };
 
-function Preview({
-  id,
-  removeQuestion,
-  editMode,
-}: QuestionData) {
+function Preview({ id, removeQuestion, editMode }: QuestionData) {
+  const [answerType, setAnswerType] = useState();
+  const [questionTxt, setQuestionTxt] = useState("");
+  const [correctAnswers, setCorrectAnswers] = useState(0);
+  const [answers, setAnswers]: any = useState([]);
+
+  let protoType: any =
+    answerType === "OpenTextQuestion"
+      ? questionTxt
+      : { questionText: questionTxt, answerOptions: [] };
+
   const [questionTest, setQuestionTest] = useState({
     name: "",
     authorId: "",
     type: "",
     difficultyLevel: "",
-    content: "" || {questionText: ""}
+    tags: [],
+    content: protoType,
   });
 
   useEffect(() => {
@@ -27,6 +46,12 @@ function Preview({
       .then((res) => res.json())
       .then((result) => {
         setQuestionTest(result);
+        setAnswerType(result.type);
+        setQuestionTxt(
+          result.type === "MultipleChoiceQuestion"
+            ? result.content.questionText
+            : result.content
+        );
       });
   }, []);
 
@@ -45,6 +70,7 @@ function Preview({
         display: "inline-block",
       }}
     >
+{console.log("hi")}
       {editMode ? (
         <ActionIcon
           color="red"
@@ -62,16 +88,71 @@ function Preview({
       )}
 
       <Title order={3}>{questionTest.name}</Title>
-      <Divider my="sm" color="blue" />
-      <Text lineClamp={2}>
-        {questionTest.type === "MultipleChoiceQuestion" ? (
-          questionTest.content.questionText
-        ) : questionTest.type === "OpenTextQuestion" ? (
-          questionTest.content
-        ) : (
-          <></>
-        )}
-      </Text>
+      <Group spacing="xs" style={{ paddingTop: 5 }}>
+        {questionTest.tags?.map((i: any) => {
+          return (
+            <Badge key={i} color="pink" variant="light">
+              {i}
+            </Badge>
+          );
+        })}
+      </Group>
+      <Divider my="xs" color="blue" />
+
+      <div data-color-mode="light">
+        <MDEditor.Markdown source={questionTxt} />
+      </div>
+
+      {questionTest.type === "MultipleChoiceQuestion" ? (
+        questionTest.content.answerOptions.map((e: any) => (
+          <Radio
+            key={e.description}
+            disabled
+            checked={e.isCorrect}
+            label={e.description}
+            value={e.description}
+            style={{ padding: 5 }}
+          />
+        ))
+      ) : (
+        <></>
+      )}
+
+      {/* {questionTest.type === "MultipleChoiceQuestion" ? (
+        (questionTest.content.answerOptions.map(
+          (i: any) => (
+            i.isCorrect
+              ? setCorrectAnswers(correctAnswers + 1)
+              : console.log(""),
+            setAnswers([...answers, i])
+          )
+        ),
+        correctAnswers > 1
+          ? answers.map((e: any) => (
+              <Checkbox
+                key={e.description}
+                disabled
+                checked={e.isCorrect}
+                label={e.description}
+                value={e.description}
+                style={{ padding: 5 }}
+              />
+            ))
+          : answers.map((e: any) => (
+              <Radio
+                key={e.description}
+                disabled
+                checked={e.isCorrect}
+                label={e.description}
+                value={e.description}
+                style={{ padding: 5 }}
+              />
+            )))
+      ) : questionTest.type === "OpenTextQuestion" ? (
+        <></>
+      ) : (
+        <></>
+      )} */}
 
       <Button
         size="xs"
