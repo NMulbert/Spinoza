f1d317fc38b38bf02a824bdff1a78c7d34ea72a0
Firas Naarani <89148516+FirasNaarani@users.noreply.github.com>
2022-05-26 02:39:26 +0300

    Advance Tests Automation

--- Spinoza_Automation/Spinoza/src/Test1.java
+++ Spinoza_Automation/Spinoza/src/Test1.java
@@ -1,4 +1,6 @@
+import java.time.Duration;
 import java.util.Date;
+import java.util.NoSuchElementException;
 import java.util.concurrent.TimeUnit;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
@@ -6,44 +8,165 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.FluentWait;
+import org.openqa.selenium.support.ui.Wait;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+
+import com.google.common.base.Function;
 
 public class Test1 {
 
 	public static void main(String[] args) throws InterruptedException {
 		// Invoke Browser
-		// Test Create Test
+		// Tests Create Test
 
 		System.setProperty("webdriver.chrome.driver", ".\\lib\\chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
-		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
 
 		driver.manage().window().maximize();
 		driver.get("http://localhost:3000/");
-
-		System.out.println("TEST START - TEST CREATE");
-		CreateNewTest(driver);
-		System.out.println("TEST END - TEST CREATE");
 		
+		System.out.println("TEST START - TEST'S CREATE");
+
+		System.out.println("-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-");
+
+		System.out.println("TEST START - TEST 1");
+		CreateNewTest(driver);
+		System.out.println("TEST END - TEST 1");
+
+		System.out.println("-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-");
+
+		System.out.println("TEST START - TEST 2");
+		CreateNewTest_Test1(driver);
+		System.out.println("TEST END - TEST 2");
+
+		System.out.println("-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-");
+
+		System.out.println("TEST START - TEST 3");
+		CreateNewTest_Test2(driver);
+		System.out.println("TEST END - TEST 3");
+
+		System.out.println("-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-");
+
+		System.out.println("TEST END - TEST 4");
+		CreateNewTest_Test3(driver);
+		System.out.println("TEST END - TEST 4");
+
+		System.out.println("-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-");
+
+		System.out.println("TEST END - TEST'S CREATE");
+
 		Thread.sleep(5000);
 		driver.quit();
 	}
 
-	public static void CreateNewTest(WebDriver driver) throws InterruptedException {
-		driver.findElement(By.xpath("//span[contains(text(),'Tests')]")).click();
-		driver.findElement(By.xpath("//span[contains(text(),'NEW TEST')]")).click();
+	public static void CreateNewTest(WebDriver driver) {
+		try {
+			// CREATE TEST AND CHECK TITLE CONFLICT
+			driver.findElement(By.xpath("//span[contains(text(),'Tests')]")).click();
+			Thread.sleep(5000);
+			driver.findElement(By.xpath("//span[contains(text(),'NEW TEST')]")).click();
 
-		// TITLE
-		AddTitle(driver);
+			AddTitle(driver, "Test Conflict");
 
-		// DESCRIPTION
-		AddDescription(driver);
+			driver.findElement(By.cssSelector(
+					"button[class='mantine-Button-gradient mantine-Button-root mantine-Group-child mantine-e8vnjr']"))
+					.click();
 
-		// TAGS
-		AddTags(driver);
+			WebDriverWait wait = new WebDriverWait(driver, 15);
+			wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[@role='alert']")));
 
-		// SAVE
-		SaveTest(driver);
+			String alert = driver.findElement(By.xpath("//div[@role='alert']")).getText();
+
+			if (!alert.contains("Conflict")) {
+				driver.findElement(By.cssSelector(
+						"button[class='mantine-Button-gradient mantine-Button-root mantine-Group-child mantine-e8vnjr']"))
+						.click();
+				CreateTest3_Check(driver);
+
+			} else {
+				System.out.println("Create Test = SUCCESS");
+
+			}
+
+			driver.findElement(By.xpath("//span[contains(text(),'Tests')]")).click();
+
+		} catch (Exception e) {
+			System.out.println("Error = CreateNewTest");
+			System.out.println(e);
+		}
+
+	}
+
+	public static void CreateNewTest_Test1(WebDriver driver) {
+		try {
+			// CREATE TEST WITHOUT DATA
+			driver.findElement(By.xpath("//span[contains(text(),'Tests')]")).click();
+			driver.findElement(By.xpath("//span[contains(text(),'NEW TEST')]")).click();
+
+			driver.findElement(By.cssSelector(
+					"button[class='mantine-Button-gradient mantine-Button-root mantine-Group-child mantine-e8vnjr']"))
+					.click();
+
+			CreateTest1_Check(driver);
+
+			driver.findElement(By.xpath("//span[contains(text(),'Tests')]")).click();
+
+		} catch (Exception e) {
+			System.out.println("Error = CreateNewTest_Test1");
+			System.out.println(e);
+		}
+	}
+
+	public static void CreateNewTest_Test2(WebDriver driver) {
+		try {
+			// CREATE TEST WITH TITLE ONLY
+			driver.findElement(By.xpath("//span[contains(text(),'Tests')]")).click();
+			driver.findElement(By.xpath("//span[contains(text(),'NEW TEST')]")).click();
+
+			AddTitle(driver);
+
+			driver.findElement(By.cssSelector(
+					"button[class='mantine-Button-gradient mantine-Button-root mantine-Group-child mantine-e8vnjr']"))
+					.click();
+
+			WebDriverWait wait = new WebDriverWait(driver, 15);
+			wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[@role='alert']")));
+
+			CreateTest2_Check(driver);
+
+			driver.findElement(By.xpath("//span[contains(text(),'Tests')]")).click();
+
+		} catch (Exception e) {
+			System.out.println("Error = CreateNewTest_Test2");
+			System.out.println(e);
+		}
+	}
+
+	public static void CreateNewTest_Test3(WebDriver driver) {
+		try {
+			// CREATE TEST WITH ALL DATA FIELDS
+			driver.findElement(By.xpath("//span[contains(text(),'Tests')]")).click();
+			driver.findElement(By.xpath("//span[contains(text(),'NEW TEST')]")).click();
+
+			// TITLE
+			AddTitle(driver);
+
+			// DESCRIPTION
+			AddDescription(driver);
+
+			// TAGS
+			AddTags(driver);
+
+			// SAVE
+			SaveTest(driver);
+
+		} catch (Exception e) {
+			System.out.println("Error = CreateNewTest_Test3");
+			System.out.println(e);
+		}
 	}
 
 	private static void AddTitle(WebDriver driver) {
@@ -54,6 +177,19 @@ public class Test1 {
 			TestTitle.clear();
 			TestTitle.sendKeys("Automation TEST | " + s);
 			System.out.println("TITLE = 'NEW TEST' | " + s);
+
+		} catch (Exception e) {
+			System.out.println("Error = Test Title");
+			System.out.println(e);
+		}
+	}
+
+	private static void AddTitle(WebDriver driver, String title) {
+		try {
+			WebElement TestTitle = driver.findElement(By.xpath("//input[@placeholder='Test title']"));
+			TestTitle.clear();
+			TestTitle.sendKeys("Automation TEST | " + title);
+			System.out.println("TITLE = 'NEW TEST' | " + title);
 
 		} catch (Exception e) {
 			System.out.println("Error = Test Title");
@@ -89,31 +225,61 @@ public class Test1 {
 			System.out.println(e);
 		}
 	}
-	
+
 	private static void SaveTest(WebDriver driver) {
 		try {
 			driver.findElement(By.cssSelector(
 					"button[class='mantine-Button-gradient mantine-Button-root mantine-Group-child mantine-e8vnjr']"))
-			.click();
-			
+					.click();
+
 			WebDriverWait wait = new WebDriverWait(driver, 15);
 			wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[@role='alert']")));
-			
+
 			String alert = driver.findElement(By.xpath("//div[@role='alert']")).getText();
-			
-			if(alert.contains("InternalServerError")) {
-				System.out.println("TEST FAILED");
+
+			if (alert.contains("InternalServerError")) {
+				System.out.println("Create Test = FAILED");
+			} else {
+				System.out.println("Create Test = SUCCESS");
 			}
-			else {
-				System.out.println("TEST SUCCESS");
-			}
-			
+
 			driver.findElement(By.xpath("//span[contains(text(),'Tests')]")).click();
-			
+
 		} catch (Exception e) {
 			System.out.println("Error = Save As Draft");
 			System.out.println(e);
 		}
 	}
 
+	private static void CreateTest1_Check(WebDriver driver) {
+		try {
+			Assert.assertTrue(
+					driver.findElement(By.cssSelector(".mantine-Text-root.mantine-TextInput-error.mantine-sy7825"))
+							.getText().contains("required field"));
+			System.out.println("Create Test1 = SUCCESS");
+		} catch (Exception e) {
+			System.out.println("Create Test1 = FAILED");
+			System.out.println(e);
+		}
+	}
+
+	private static void CreateTest2_Check(WebDriver driver) {
+		try {
+			Assert.assertTrue(driver.findElement(By.xpath("//div[@role='alert']")).getText().contains("created"));
+			System.out.println("Create Test2 = SUCCESS");
+		} catch (Exception e) {
+			System.out.println("Create Test2 = FAILED");
+			System.out.println(e);
+		}
+	}
+
+	private static void CreateTest3_Check(WebDriver driver) {
+		try {
+			Assert.assertTrue(driver.findElement(By.xpath("//div[@role='alert']")).getText().contains("Conflict"));
+			System.out.println("Create Test3 = SUCCESS");
+		} catch (Exception e) {
+			System.out.println("Create Test3 = FAILED");
+			System.out.println(e);
+		}
+	}
 }
--- Spinoza_Automation/Spinoza/src/Test4.java
+++ Spinoza_Automation/Spinoza/src/Test4.java
@@ -1,165 +1,83 @@
-import java.util.Date;
-import java.util.Random;
 import java.util.concurrent.TimeUnit;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
 
 public class Test4 {
 
 	public static void main(String[] args) throws InterruptedException {
 		// Invoke Browser
-		// Test Create Questions
+		// Tests Search Test
 
 		System.setProperty("webdriver.chrome.driver", ".\\lib\\chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
-		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
 
 		driver.manage().window().maximize();
 		driver.get("http://localhost:3000/");
 
-		System.out.println("TEST START - QUESTION CREATE");
-		CreateNewQuestion(driver);
-		System.out.println("TEST END - QUESTION CREATE");
+		System.out.println("TEST START - TEST'S SEARCH");
 
-		Thread.sleep(5000);
+		System.out.println("-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-");
+
+		System.out.println("TEST START - TEST 1");
+		SearchTestsByTag(driver, "Python");
+		System.out.println("TEST END - TEST 1");
+
+		System.out.println("-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-");
+
+		System.out.println("TEST END - TEST'S SEARCH");
+
+		Thread.sleep(3000);
 		driver.quit();
 	}
 
-	public static void CreateNewQuestion(WebDriver driver) throws InterruptedException {
-		driver.findElement(By.xpath("//span[contains(text(),'Questions')]")).click();
-		driver.findElement(By.xpath("//span[contains(text(),'NEW QUESTION')]")).click();
-
-		// TITLE
-		AddTitle(driver);
-
-		// TAGS
-		AddTags(driver);
-
-		// DESCRIPTION
-		AddDescription(driver);
-
-		// ANSWER TYPE
-		AddAnswerType(driver);
-
-		// SELECT TYPE
-		AddSelectLevel(driver);
-
-		// SAVE
-		SaveTest(driver);
-	}
-
-	private static void AddTitle(WebDriver driver) {
+	public static void SearchTestsByTag(WebDriver driver, String tag) {
 		try {
-			Date date = new Date();
-			String s = date.toLocaleString();
-			WebElement TestTitle = driver.findElement(By.xpath("//input[@placeholder='Question Name']"));
-			TestTitle.clear();
-			TestTitle.sendKeys("Automation QUESTION | " + s);
-			System.out.println("TITLE = 'NEW QUESTION' | " + s);
+			// GET TESTS COUNT BY TAG
+			int count = GetTestsByTag(driver, tag);
 
+			// SEARCH TESTS BY TAG
+			WebElement Search = driver.findElement(By.xpath("//input[@placeholder='Select tags']"));
+			Search.sendKeys(tag, Keys.ENTER);
+
+			Thread.sleep(2500);
+			int result = driver.findElements(By.xpath("//div[@class='mantine-Col-root mantine-1akkebb']")).size() - 1;
+
+			SearchTestsByTag_Check(count,result);
+			
+			driver.findElement(By.xpath("//button[@class='mantine-ActionIcon-transparent mantine-ActionIcon-root mantine-MultiSelect-defaultValueRemove mantine-cc6qwu']")).click();
+			Search.click();
+			
 		} catch (Exception e) {
-			System.out.println("Error = Question Title");
+			System.out.println("Search Tests By Tag = FAILED");
 			System.out.println(e);
 		}
 	}
 
-	private static void AddTags(WebDriver driver) {
+	private static int GetTestsByTag(WebDriver driver, String tag) {
 		try {
-			WebElement Tags = driver.findElement(By.xpath("//input[@placeholder='Select tags']"));
-			Tags.sendKeys("React", Keys.ENTER);
-			Tags.sendKeys("C#", Keys.ENTER);
-			Tags.sendKeys("JavaScript", Keys.ENTER);
-			Tags.sendKeys("Python", Keys.ENTER);
-			System.out.println("TAGS = ['React','C#','JavaScript','Python']");
+			// GET TESTS BY TAG
+			return driver.findElements(By.xpath("//span[contains(text(),'" + tag + "')]")).size();
 
 		} catch (Exception e) {
-			System.out.println("Error = Tags");
+			System.out.println("Get Tests By Tag = FAILED");
 			System.out.println(e);
+			return -1;
 		}
 	}
 
-	private static void AddDescription(WebDriver driver) {
+	private static void SearchTestsByTag_Check(int count, int result) {
 		try {
-			WebElement Description = driver.findElement(By.cssSelector(".w-md-editor-text-input"));
-			Description.click();
-			Description.clear();
-			Description.sendKeys("QUESTION DESCRIPTION");
-			System.out.println("DESCRIPTION = 'QUESTION DESCRIPTION'");
-
+			Assert.assertEquals(count, result);
+			System.out.println("Search Tests By Tag = SUCCESS");
 		} catch (Exception e) {
-			System.out.println("Error = Description");
+			System.out.println("Search Tests By Tag = FAILED");
 			System.out.println(e);
 		}
 	}
-
-	private static void AddAnswerType(WebDriver driver) {
-		try {
-			// CHOICE 'TEXT' TYPE
-			driver.findElement(By.xpath("//label[contains(text(),'Text')]")).click();
-			System.out.println("ANSWER TYPE = 'TEXT'");
-
-		} catch (Exception e) {
-			System.out.println("Error = Answer Type");
-			System.out.println(e);
-		}
-	}
-
-	private static void AddSelectLevel(WebDriver driver) {
-		try {
-			// GENERATE RANDOM NUMBER FOR LEVEL
-			int levels = driver.findElements(By.xpath("//input[@class='mantine-1ll72zi mantine-RadioGroup-radio']"))
-					.size();
-			Random rnd = new Random();
-			int select = rnd.nextInt((levels - (levels - 1)), levels);
-
-			driver.findElement(By.xpath("(//input[@class='mantine-1ll72zi mantine-RadioGroup-radio'])[" + select + "]"))
-					.click();
-			System.out.println("SELECT LEVEL = 'QUESTION LEVEL' (" + select + ")");
-
-		} catch (Exception e) {
-			System.out.println("Error = Select Type");
-			System.out.println(e);
-		}
-	}
-
-	private static void SaveTest(WebDriver driver) {
-		try {
-			// SAVE AS DRAFT
-			driver.findElement(By.cssSelector(
-					"button[class='mantine-Button-gradient mantine-Button-root mantine-Group-child mantine-1m2tbz']"))
-					.click();
-
-			WebDriverWait wait = new WebDriverWait(driver, 15);
-			wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("div[role='alert']")));
-
-			String alert = driver.findElement(By.cssSelector("div[role='alert']")).getText();
-
-			if (alert.contains("InternalServerError")) {
-				System.out.println("TEST FAILED");
-				driver.findElement(By.cssSelector(
-						"button[class='mantine-ActionIcon-hover mantine-ActionIcon-root mantine-Modal-close mantine-vao037']"))
-						.click();
-			} else {
-				System.out.println("TEST SUCCESS");
-				driver.findElement(By.cssSelector(
-						"button[class='mantine-ActionIcon-hover mantine-ActionIcon-root mantine-Modal-close mantine-vao037']"))
-						.click();
-
-				// RE-ENTER TO QUESTIONS
-				driver.findElement(By.xpath("//span[contains(text(),'Tests')]")).click();
-				driver.findElement(By.xpath("//span[contains(text(),'Questions')]")).click();
-			}
-
-		} catch (Exception e) {
-			System.out.println("Error = Save As Draft");
-			System.out.println(e);
-		}
-
-	}
-
 }
